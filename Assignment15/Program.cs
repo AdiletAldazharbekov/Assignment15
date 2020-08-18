57f16076224da02e1c4d231189687df19b6f28c4
AdiletAldazharbekov <aldajarbekov@gmail.com>
2020-08-19 01:34:03 +0600

    Finaly Test

--- Assignment15/Program.cs
+++ Assignment15/Program.cs
@@ -14,11 +14,24 @@ namespace Assignment15
             dictionary.Add("1", "First");
             dictionary.Add("2", "Second");
             dictionary.Add("1", "Third");
+            dictionary.Add("3", "Third");
+
+            Console.WriteLine();
 
             Console.WriteLine(dictionary.GetValue("3"));
 
-            dictionary.Remove("3");
+            Console.WriteLine();
+            Console.WriteLine(dictionary.GetValue("4"));
 
+            Console.WriteLine();
+
+            dictionary.Remove("3");
+            Console.WriteLine();
+
+            dictionary.Remove("5");
+
+
+            Console.WriteLine();
             dictionary.Print();
 
         }
